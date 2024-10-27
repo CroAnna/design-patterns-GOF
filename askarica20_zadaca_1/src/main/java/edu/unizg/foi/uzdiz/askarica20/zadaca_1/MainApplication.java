4617e449fbb2c5e1c9c63fc24427590841f8a390
Ana Skarica <ana.skarica.as@gmail.com>
2024-10-27 14:46:43 +0100

    Initial commit

---
+++ askarica20_zadaca_1/src/main/java/edu/unizg/foi/uzdiz/askarica20/zadaca_1/MainApplication.java
@@ -0,0 +1,11 @@
+package edu.unizg.foi.uzdiz.askarica20.zadaca_1;
+
+public class MainApplication {
+  // ovo bu glavna tvrtka
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("-----------test-- promjena ---------------");
+  }
+
+}
