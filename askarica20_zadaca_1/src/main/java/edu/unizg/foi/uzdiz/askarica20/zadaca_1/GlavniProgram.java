e5e2b715aed4427bbe0256b5f0fe691366a78383
Ana Skarica <ana.skarica.as@gmail.com>
2024-10-28 14:48:20 +0100

    Add methods to find invalid arguments

--- askarica20_zadaca_1/src/main/java/edu/unizg/foi/uzdiz/askarica20/zadaca_1/GlavniProgram.java
+++ askarica20_zadaca_1/src/main/java/edu/unizg/foi/uzdiz/askarica20/zadaca_1/GlavniProgram.java
@@ -1,13 +1,53 @@
 package edu.unizg.foi.uzdiz.askarica20.zadaca_1;
 
 public class GlavniProgram {
-  // ovo bu glavna tvrtka
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-    System.out.println("-----------test----------------");
+    try {
+      if (!provjeriSastavArgumenata(args)) {
+        System.out.println("Neispravni argumenti.");
+      } else {
+        System.out.println("-----------ok argumenti----------------");
+      }
+    } catch (IllegalArgumentException e) {
+      System.out.println("Greška: " + e.getMessage());
+    }
   }
 
+  public static boolean provjeriSastavArgumenata(String[] args) {
+    int zsFlag = 0, zpsFlag = 0, zkFlag = 0;
 
+    if (args.length != 6) {
+      throw new IllegalArgumentException("Broj argumenata nije dobar.");
+    }
 
+    for (int i = 0; i < args.length; i += 2) {
+      String zastavica = args[i];
+      String datoteka = args[i + 1];
+
+      switch (zastavica) {
+        case "--zs":
+          zsFlag++;
+          break;
+        case "--zps":
+          zpsFlag++;
+          break;
+        case "--zk":
+          zkFlag++;
+          break;
+        default:
+          return false; // Nepoznata zastavica
+      }
+
+      if (datoteka.isEmpty()) {
+        throw new IllegalArgumentException("Datoteka u argumentima je prazna.");
+      }
+    }
+
+    if (zsFlag == 1 && zpsFlag == 1 && zkFlag == 1) {
+      return true;
+    } else {
+      throw new IllegalArgumentException("Nema po jedan dokument za svaku zastavicu.");
+    }
+  }
 }
